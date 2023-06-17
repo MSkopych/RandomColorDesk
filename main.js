acf3e481c0ad8f5059eff08baf51e68e957c1fd7
MSkopych <87950220+MSkopych@users.noreply.github.com>
2023-06-17 14:15:09 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const board= document.querySelector('#board');
 const SQUARES_NUMBER = 500;
-const colors = [#];
+const colors = ['#E635CB','#E6B363','#E64CD0','#60E635','#4099E6'];
 
 for(let i = 0; i < SQUARES_NUMBER; i++){
 const square = document.createElement('div');
@@ -14,3 +14,16 @@ board.append(square);
 
 }
 
+function setColor(element){
+    const color = getRandomColor();
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+function removeColor(element){
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #1d1d1d`;
+}
+function getRandomColor(){
+   const index = Math.floor(Math.random() * colors.length)
+   return colors[index];
+}
